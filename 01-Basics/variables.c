ef9057edac00be62bfd5bd30cc956f34c9f77e85
Aryan Chaudhari <04.aryanc@gmail.com>
2026-08-15 01:30:56 +0530

    Variables in C

---
+++ 01-Basics/variables.c
@@ -0,0 +1,15 @@
+//Studio.h is a Standard input output header file.
+#include <stdio.h>
+
+
+int main() {
+    //This will make 2 variables a and b;
+    int a,b;
+
+    //We are giving them a value
+    a = 10;
+    b = 20;
+
+    //lets print them
+    printf("The value of a is : %d\nThe value of b is : %d\n", a, b);
+}
